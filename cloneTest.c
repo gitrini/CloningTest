3a137507033ee7cdc37205a38536ff466a24258a
gitrini <shinplest@kakao.com>
2021-03-29 18:31:05 +0900

    initail commit

---
+++ cloneTest.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+	printf("설마 이 소스를 보러 오시는 분도 있을까요?\n");
+	printf("깃린이 코스 화이팅!\n"); 
+	return 0;
+}
---
+++ cloneTest2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+	printf("안녕하세요 반갑습니다.\n");
+	printf("이 소스까지 확인하신 분은\n");
+	printf("이 리포지토리에 PR을 넣어주세요.\n"); 
+	printf("PR은 수업 후반부에 배울건데요, 끝까지 완강하시고 PR넣어주세요. ㅎ\n"); 
+
+	return 0;
+}
